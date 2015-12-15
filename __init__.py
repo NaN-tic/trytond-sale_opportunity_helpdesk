2dca47f462e6db03591d37353241556994ed4078
resteve <resteve@zikzakmedia.com>
2015-12-15 17:14:48 +0100

    Getmail kind

--- __init__.py
+++ __init__.py
@@ -3,10 +3,11 @@
 # copyright notices and license terms.
 from trytond.pool import Pool
 from .helpdesk import *
-
+from .getmail import *
 
 def register():
     Pool.register(
         Helpdesk,
         SaleOpportunityHelpdesk,
+        GetmailServer,
         module='sale_opportunity_helpdesk', type_='model')
